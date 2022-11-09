2427eab489fa41885e387da25af5ef8e22eff54e
adilsonfuta <adilson.futa@jupiter.co.ao>
2022-11-09 15:08:56 +0000

    ajustes de fluxos

---
+++ src/data.js
@@ -0,0 +1,12 @@
+module.exports = () =>{
+
+    const data ={
+        products: []
+    }
+
+    let tam = 1000;
+    for (let index = 0; index < tam; index++) {
+        const element = array[index];
+        
+    }
+}
