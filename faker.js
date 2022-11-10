7fb429d4d1920e9d44247ac8b4d48866567031ac
adilsonfuta <adilson.futa@jupiter.co.ao>
2022-11-10 12:18:03 +0000

    ajustes de scripts

--- faker.js
+++ faker.js
@@ -1,34 +1,26 @@
-import { faker } from "@faker-js/faker";
+// import { faker } from "@faker-js/faker";
 // import { faker } from "@faker-js/faker/locale/pt_PT";
-// const faker = require('@faker-js/faker');
 
 module.exports = () => {
-
-
-const tamanho = 10;
-const  dataItem ={
+  const tamanho = 10;
+  const dataItem = {
     products: []
+  };
+
+//   const faker = require("@faker-js/faker");
+
+  for (let i = 0; i < tamanho; i++) {
+    dataItem.products.push({
+      id: faker.datatype.uuid(),
+      title: `Product ${faker.datatype.uuid()}`,
+      category: faker.internet.userName,
+      price: faker.datatype.number(),
+      description: `This is description about ${faker.datatype.uuid()}`
+    });
+  }
+
+  console.log(dataItem);
+  return dataItem;
 };
 
-for (let i = 0; i < tamanho; i++) {
-    dataItem.products.push({
-        id: faker.datatype.uuid(),
-        title: `Product ${faker.datatype.uuid()}`,
-        category: faker.internet.userName,
-        price: faker.datatype.number(),
-        description: `This is description about ${faker.datatype.uuid()}`  
-    });
-}
-
-return dataItem;
-} 
-
-
-
-
-
-
 // return dataItem;
-
-
-
